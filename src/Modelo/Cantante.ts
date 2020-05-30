ec37adedcfc1c7c4943b7a1f7a6fe5ca0c96aa74
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-30 12:23:49 -0300

    test

---
+++ src/Modelo/Cantante.ts
@@ -0,0 +1,4 @@
+export class Cantante {
+   artist: string;
+   country: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cantante } from 'src/Modelo/Cantante';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngstyle';
+
+  cantantes: Cantante[] = [
+    {
+      'artist': 'Michael Jackson',
+      'country': 'USA'
+    },
+    {
+      'artist': 'Justin Bieber',
+      'country': 'Canada'
+    },
+    {
+      'artist': 'Daddy Yankee',
+      'country': 'Purto Rico'
+    },
+    {
+      'artist': 'A R Rehman',
+      'country': 'India'
+    },
+    {
+      'artist': 'Selena Gomez',
+      'country': 'USA'
+    }
+  ];
 }
